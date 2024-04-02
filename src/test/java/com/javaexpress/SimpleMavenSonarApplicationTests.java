546cbd6b7fd5c2b4b61f89da32f029c546e80957
javaexpresschannel <javaexpresschannel@gmail.com>
2024-04-02 17:57:50 +0530

    sonar test

--- src/test/java/com/javaexpress/SimpleMavenSonarApplicationTests.java
+++ src/test/java/com/javaexpress/SimpleMavenSonarApplicationTests.java
@@ -3,10 +3,10 @@ package com.javaexpress;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SimpleMavenSonarApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
